0c0c202f89efd19837e815db9ecffdc093da2f79
poirazis <poirazis@gmail.com>
2023-05-09 16:55:28 +0300

    First working version

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,12 +1,95 @@
 <script>
   import { getContext } from "svelte"
 
-  export let text
-
-  const { styleable } = getContext("sdk")
+  const { styleable, Provider } = getContext("sdk")
   const component = getContext("component")
+
+  export let page = 1 
+  export let pageSize = 25
+  export let pages = 10
+  export let pageButtons = 8
+
+  let startIndex = 1
+  $: lastIndex = pageButtons >= pages ? pages : pageButtons + startIndex - 3
+
+  $: dataContext = { offset: Number(pageSize * (page - 1)), limit: Number(pageSize) }
+  $: buttons = generateState( page, pageSize, pages, pageButtons)
+
+  function generateState ( ) {
+    let buttons = [] 
+    
+    if ( pageButtons > pages - startIndex ) {
+      for (var i =  startIndex; i <= pages; i++) {
+        buttons.push(i);
+      }
+    } else {
+      for (var i =  startIndex; i <= startIndex + ( pageButtons - 3 ); i++) {
+        buttons.push(i);
+      }
+        buttons.push("...");
+        buttons.push(pages);
+    }
+
+    return buttons;
+  }
+
+  function handlePrevious () {
+    page = page > 1 ? page - 1 : 1
+    if (page < startIndex) startIndex = page;  
+  }
+
+  function handleClick ( item ) {
+
+    if (item == lastIndex) {
+      startIndex = item
+    } else if ( item == startIndex && startIndex > 1) {
+      startIndex = item - 1
+    } else if ( item == pages ) { 
+      startIndex = pages - pageButtons + 1
+    } else if ( item == ",,,") {
+      return
+    }
+
+    page = item
+
+  }
+
+  function handleNext () {
+    page = page < pages ? page + 1 : pages
+    if (page > lastIndex) startIndex = page
+  }
 </script>
 
 <div use:styleable={$component.styles}>
-  This is a custom component. The text setting is: {text}.
+  <Provider data={dataContext}>
+    <slot />
+  </Provider>
+  <nav style:margin-top={"1rem"} class="spectrum-Pagination spectrum-Pagination--listing">
+
+    <button 
+      disabled={page == 1} 
+      on:click={handlePrevious} 
+      class="spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionButton--quiet">
+      <span class="spectrum-Button-label">    
+        <svg class="spectrum-Icon spectrum-UIIcon-ChevronLeft100" focusable="false" aria-hidden="true" aria-label="ChevronLeft">
+        <use xlink:href="#spectrum-css-icon-Chevron100"></use>
+      </svg></span>
+    </button>
+
+    {#each buttons as button}
+      <button 
+        on:click={ () => handleClick(button) }  
+        class:is-selected={button == page } 
+        class="spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionButton--quiet">
+        <span class="spectrum-ActionButton-label">{button}</span>
+      </button>
+    {/each}
+
+    <button disabled={page == pages} on:click={handleNext} class="spectrum-ActionButton spectrum-ActionButton--sizeM spectrum-ActionButton--quiet">
+      <svg class="spectrum-Icon spectrum-UIIcon-ChevronRight100" focusable="false" aria-hidden="true" aria-label="ChevronLeft">
+        <use xlink:href="#spectrum-css-icon-Chevron100"></use>
+      </svg>
+    </button>
+
+  </nav>
 </div>
